ddad9e146fe02701587551fa74a13f4322f6e057
chenyiyun573 <chenyiyun2000@gmail.com>
2023-03-28 13:45:09 -0700

    AlexNet

---
+++ AlexNet/AlexNetTrain.py
@@ -0,0 +1,71 @@
+import torch, os
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.datasets as datasets
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import torchvision.models as models
+from train_model import train_model
+from test_model import test_model
+from AlexNet import alexnet
+
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.ToTensor(),
+    ]),
+    'val': transforms.Compose([
+        transforms.ToTensor(),
+    ]),
+    'test': transforms.Compose([
+        transforms.ToTensor(),
+    ])
+}
+
+data_dir = 'tiny-imagenet-200/'
+num_workers = {
+    'train' : 100,
+    'val'   : 0,
+    'test'  : 0
+}
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'val','test']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=100,
+                                             shuffle=True, num_workers=num_workers[x])
+              for x in ['train', 'val', 'test']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val', 'test']}
+
+#Load AlexNet
+model_ft = alexnet()
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model_ft = model_ft.to(device)
+
+#Loss Function
+criterion = nn.CrossEntropyLoss()
+# Observe that all parameters are being optimized
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+#Train
+train_model("Alex_np",model_ft, dataloaders, dataset_sizes, criterion, optimizer_ft, num_epochs=10)
+
+
+
+
+
+if False:
+    """ The code below use pre-trained weights """
+
+    #Load AlexNet with pretrained weights
+    model_ft = alexnet(True)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model_ft = model_ft.to(device)
+    #Loss Function
+    criterion = nn.CrossEntropyLoss()
+    # Observe that all parameters are being optimized
+    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+    #Train
+    train_model("Alex",model_ft, dataloaders, dataset_sizes, criterion, optimizer_ft, num_epochs=20)
---
+++ LeNet5/README.md
@@ -0,0 +1,4 @@
+## Usage
+run train.py. It will check and download MINIST dataset to folder tmp
+
+The code of train.py works well. - 2023.3.28 Yiyun
