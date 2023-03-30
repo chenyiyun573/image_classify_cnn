1395b55d1a5fdd86c457c8407d57c746b9920771
chenyiyun573 <chenyiyun2000@gmail.com>
2023-03-30 03:08:16 -0700

    add

---
+++ AlexNet/myTrain.py
@@ -0,0 +1,85 @@
+# Import necessary libraries
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as transforms
+
+# Define data transformations
+transform = transforms.Compose([
+    transforms.RandomResizedCrop(64),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+# Load data
+train_data = ImageFolder('tiny-imagenet-200/train', transform=transform)
+train_loader = DataLoader(train_data, batch_size=64, shuffle=True, num_workers=4)
+val_data = ImageFolder('tiny-imagenet-200/val', transform=transform)
+val_loader = DataLoader(val_data, batch_size=64, shuffle=False, num_workers=4)
+
+# Define model
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.relu3 = nn.ReLU(inplace=True)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(256 * 8 * 8, 512)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.relu4 = nn.ReLU(inplace=True)
+        self.fc2 = nn.Linear(512, 200)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.pool(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        x = self.pool(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu3(x)
+        x = self.pool(x)
+
+        x = x.view(-1, 256 * 8 * 8)
+        x = self.fc1(x)
+        x = self.bn4(x)
+        x = self.relu4(x)
+        x = self.fc2(x)
+
+        return x
+
+model = Net()
+
+# Define loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Train model
+for epoch in range(10):
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        inputs, labels = data
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        if i % 100 == 99:
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 100))
